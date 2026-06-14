efd72a978a9ce3ad2fc76c120689821d540e940d
Henry Post <HenryFBP@gmail.com>
2026-06-13 22:06:54 -0500

    add ideas

--- artifacts.py
+++ artifacts.py
@@ -4,6 +4,9 @@ base file for all artifacts
 artifacts can be installed in a 5x5 body panel in the pause menu. each takes up 1 slot and can
 optionally chain with other artifacts if they support it.
 
+- user is allowed to move artifacts around by selecting them and dropping them from inventory into body panel
+  - hovered/selected artifact shows a tooltip after it's hovered over for more than 3 seconds
+
 ideas:
 
     - fractal blaster: piercing shots that trace a constantly-shifting julia set outline
---
+++ enemies.py
@@ -0,0 +1,13 @@
+"""
+
+- crawler
+  - walks back and forth
+  - turns on ledges
+
+- flyer
+  - flies in a swooping pattern
+
+- shooty flier
+  - same as flier but shoots bullets
+
+"""
