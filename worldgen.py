ca4cf6cccdd84762a7c27909dc2fd3cffeec1f7a
Henry Post <HenryFBP@gmail.com>
2026-06-13 23:22:46 -0500

    update worldgen.py

--- worldgen.py
+++ worldgen.py
@@ -9,6 +9,8 @@ worldgen:
 dungeon generation:
 - simple room based generation with chambers
 - keep it simple for now
+- chance for random artifacts to appear in the dungeon
+  - artifact pickup pauses the game and displays a message with the artifact name and description. fire to close dialogue.
 
 
 """
