430832956a033cee434a459f386dc86c73675963
Henry Post <HenryFBP@gmail.com>
2026-06-13 22:08:23 -0500

    add ideas

---
+++ worldgen.py
@@ -0,0 +1,14 @@
+"""
+
+
+worldgen:
+- perlin noise worldgen
+- path traversal algorithm guarantees it's able to be climbed across, failing worlds get rejected or regenerated
+
+
+dungeon generation:
+- simple room based generation with chambers
+- keep it simple for now
+
+
+"""
